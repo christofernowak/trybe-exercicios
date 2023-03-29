3934f2b2aad2ed3313b0cb7235738f5758725618
Christofer <christ1902@yahoo.com.br>
2023-03-29 17:48:22 -0300

    exercicios de fixar realizados

---
+++ fundamentos/secao-03-introducao-javascript/dia-02-javascript-array-e-loop-for/contador.js
@@ -0,0 +1,6 @@
+let num = 4;
+
+for (let index = 1; index <=10; index += 1 ) {
+
+console.log(num * index);
+}
---
+++ fundamentos/secao-03-introducao-javascript/dia-02-javascript-array-e-loop-for/saudacion.js
@@ -0,0 +1,5 @@
+let nomes = ['Paula', 'Roberto','Amanda','Cleide']
+
+for(let index =0; index<nomes.length; index +=1 ){
+console.log('Olá '+nomes[index]);
+}
