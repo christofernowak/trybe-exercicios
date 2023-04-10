3fc421c283abe8b8e96f68a44bc2e4a823f52377
Christofer <christ1902@yahoo.com.br>
2023-04-10 15:20:29 -0300

    exercicios

---
+++ fundamentos/secao-03-introducao-javascript/dia-05-javascript-es6-let-const-arrow-functions/projeto.js
@@ -0,0 +1,8 @@
+function compareTrue (verdade, mentira) {
+    const girafa = true;
+    const elefante = true;
+    const macaco = false;
+    if (elefante === true && girafa === true) {
+        return girafa;
+    }
+}
